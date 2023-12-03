7b445d97d0eac821fa4b38244b6eb1409ff484cc
Eduard de Jong <86660027+eduarddejong@users.noreply.github.com>
2023-12-03 23:20:04 +0100

    Implement Windows Hook
    Use hook for CapsLock interception instead of the registered hotkey because it worked really buggy, while this works very well.
    
    Still use the registered hotkeys for the other actions where this is sufficient.Implement Windows Hook
    
    Use hook for CapsLock interception instead of the registered hotkey because it worked really buggy, while this works very well.
    
    Still use the registered hotkeys for the other actions where this is sufficient.

--- EduardDeJong.HotKeys/MainForm.cs
+++ EduardDeJong.HotKeys/MainForm.cs
@@ -7,35 +7,50 @@ using System.Windows.Forms;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace EduardDeJong.HotKeys;
 
 public unsafe partial class MainForm : Form
 {
-    private volatile bool _handlingHotKey = false;
+    private static readonly HookKey[] _hookKeys = [
+        new(ModifiersState: [(key: Keys.Shift, state: new(Down: false))],
+            Vk: Keys.CapsLock,
+            Action: (out bool intercept) =>
+            {
+                intercept = true;
+                SendKeys.SendWait("{Esc}");
+            }),
 
-    private readonly HotKey[] _hotKeys = [
-        new(0x00000000, Keys.CapsLock,
-            () => SendKeys.SendWait("{CapsLock}{Esc}")),
+        new(ModifiersState: [(key: Keys.Shift, state: new(Down: true))],
+            Vk: Keys.CapsLock,
+            // Do nothing
+            Action: (out bool intercept) => intercept = false),
+    ];
 
-        new(HOT_KEY_MODIFIERS.MOD_SHIFT, Keys.CapsLock,
-            () => { /* No action needed */}),
+    private static readonly HotKey[] _hotKeys = [
+        new(FsModifiers: HOT_KEY_MODIFIERS.MOD_WIN,
+            Vk: Keys.Oemtilde,
+            Action: () => SendKeyboardInputs([
+                new(wVk: (VIRTUAL_KEY)Keys.LWin),
+                new(wVk: (VIRTUAL_KEY)Keys.D1)])),
 
-        new(HOT_KEY_MODIFIERS.MOD_WIN, Keys.Oemtilde,
-            () => SendKeyboardInputs([
-                new(wVk: VIRTUAL_KEY.VK_LWIN),
-                new(wVk: VIRTUAL_KEY.VK_1)])),
-
-        new(HOT_KEY_MODIFIERS.MOD_WIN | HOT_KEY_MODIFIERS.MOD_SHIFT, Keys.B,
-            () => ExecuteCommand(
+        new(FsModifiers: HOT_KEY_MODIFIERS.MOD_WIN | HOT_KEY_MODIFIERS.MOD_SHIFT,
+            Vk: Keys.B,
+            Action: () => ExecuteCommand(
                 fileName: "firefox",
                 arguments: Array.Empty<string>())),
 
-        new(HOT_KEY_MODIFIERS.MOD_WIN, Keys.C,
-            () => ExecuteCommand(
+        new(FsModifiers: HOT_KEY_MODIFIERS.MOD_WIN,
+            Vk: Keys.C,
+            Action: () => ExecuteCommand(
                 fileName: "code",
                 arguments: Array.Empty<string>()))
     ];
+
+    private HHOOK _windowsHook;
+
+    private volatile bool _handlingHotKey = false;
 
     public MainForm()
     {
@@ -50,9 +65,15 @@ public unsafe partial class MainForm : Form
     {
         base.OnLoad(e);
 
+        using ProcessModule? module = Process.GetCurrentProcess().MainModule;
+        _windowsHook = PInvoke.SetWindowsHookEx(
+            idHook: WINDOWS_HOOK_ID.WH_KEYBOARD_LL,
+            lpfn: HookProc,
+            hmod: (HINSTANCE)(module?.BaseAddress ?? nint.Zero),
+            dwThreadId: 0);
+
         foreach ((HotKey hotKey, int index) in _hotKeys.Select((hotKey, index) => (hotKey, index)))
         {
-            PInvoke.UnregisterHotKey((HWND)Handle, index);
             PInvoke.RegisterHotKey((HWND)Handle, index, hotKey.FsModifiers, (uint)hotKey.Vk);
         }
 
@@ -65,6 +86,8 @@ public unsafe partial class MainForm : Form
         {
             PInvoke.UnregisterHotKey((HWND)Handle, index);
         }
+
+        PInvoke.UnhookWindowsHookEx(_windowsHook);
 
         base.OnFormClosing(e);
     }
@@ -94,6 +117,88 @@ public unsafe partial class MainForm : Form
         }
     }
 
+    private LRESULT HookProc(int code, WPARAM wParam, LPARAM lParam)
+    {
+        if (code >= 0)
+        {
+            KEYBDINPUT keybdInput = *(KEYBDINPUT*)(nint)lParam;
+            switch ((uint)wParam)
+            {
+                case PInvoke.WM_KEYDOWN or PInvoke.WM_SYSKEYDOWN:
+                    foreach ((HookKey hookKey, int hookKeyIndex) in _hookKeys.Select((hookKey, index) => (hookKey, index)))
+                    {
+                        if ((Keys)(int)keybdInput.wVk == hookKey.Vk)
+                        {
+                            if (!hookKey.ModifiersState.Any(x =>
+                            {
+                                (Keys key, HookKeyModifierState modifierState) = x;
+                                int keyState = key switch
+                                {
+                                    Keys.Control =>
+                                        PInvoke.GetKeyState((int)Keys.LControlKey)
+                                        | PInvoke.GetKeyState((int)Keys.RControlKey),
+
+                                    Keys.Shift =>
+                                        PInvoke.GetKeyState((int)Keys.LShiftKey)
+                                        | PInvoke.GetKeyState((int)Keys.RShiftKey),
+
+                                    Keys.Alt =>
+                                        PInvoke.GetKeyState((int)Keys.LMenu)
+                                        | PInvoke.GetKeyState((int)Keys.RMenu),
+
+                                    _ => PInvoke.GetKeyState((int)key)
+                                };
+                                if (modifierState.Down is bool modifierStateDown)
+                                {
+                                    bool keyStateDown = (keyState & 0x8000) == 0x8000;
+                                    if (modifierStateDown != keyStateDown)
+                                    {
+                                        return true;
+                                    }
+                                }
+                                if (modifierState.Toggled is bool modifierStateToggled)
+                                {
+                                    bool keyStateToggled = (keyState & 0x0001) == 0x0001;
+                                    if (modifierStateToggled != keyStateToggled)
+                                    {
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            }))
+                            {
+#if DEBUG
+                                Debug.WriteLine($"""
+                                    {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {nameof(HotKey)} #{hookKeyIndex}
+                                    ({nameof(hookKey.ModifiersState)}: [{string.Join(" | ",
+                                        hookKey.ModifiersState
+                                        .Select(modifiersState =>
+                                        {
+                                            (Keys key, HookKeyModifierState state) = modifiersState;
+                                            string modifierStateDescription = string.Join(", ",
+                                                (state.Down is bool stateDown ? $"{nameof(state.Down)}: {stateDown}" : string.Empty) +
+                                                (state.Toggled is bool stateToggled ? $"{nameof(state.Toggled)}: {stateToggled}" : string.Empty));
+                                            return $"{nameof(key)}: {key}, {nameof(state)}: ({modifierStateDescription})";
+                                        })
+                                        .DefaultIfEmpty(""))}],
+                                    {nameof(hookKey.Vk)}: {hookKey.Vk}) pressed.
+                                    """.ReplaceLineEndings(" "));
+#endif
+                                hookKey.Action(out bool intercept);
+                                if (intercept)
+                                {
+                                    return (LRESULT)(-1);
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+
+        return PInvoke.CallNextHookEx(_windowsHook, code, wParam, lParam);
+    }
+
     protected override void WndProc(ref Message m)
     {
         if (!_handlingHotKey && (uint)m.Msg is PInvoke.WM_HOTKEY)
@@ -101,18 +206,18 @@ public unsafe partial class MainForm : Form
             _handlingHotKey = true;
             int hotKeyId = m.WParam.ToInt32();
             HotKey hotKey = _hotKeys[hotKeyId];
-            hotKey.Action();
 #if DEBUG
             Debug.WriteLine($"""
                 {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {nameof(HotKey)} #{hotKeyId}
                 ({nameof(hotKey.FsModifiers)}: {string.Join(" | ",
                     Enum.GetValues<HOT_KEY_MODIFIERS>()
-                    .Where(x => hotKey.FsModifiers.HasFlag(x))
-                    .Select(x => Enum.GetName(x))
+                    .Where(value => hotKey.FsModifiers.HasFlag(value))
+                    .Select(value => Enum.GetName(value))
                     .DefaultIfEmpty($"0x{hotKey.FsModifiers:x}"))},
                 {nameof(hotKey.Vk)}: {hotKey.Vk}) pressed.
                 """.ReplaceLineEndings(" "));
 #endif
+            hotKey.Action();
             _handlingHotKey = false;
         }
         else
@@ -173,6 +278,17 @@ public unsafe partial class MainForm : Form
         };
         process.Start();
     }
+
+    private sealed record class HookKey(
+        (Keys key, HookKeyModifierState state)[] ModifiersState,
+        Keys Vk,
+        HookKeyAction Action);
+
+    private record struct HookKeyModifierState(
+        bool? Down = null,
+        bool? Toggled = null);
+
+    private delegate void HookKeyAction(out bool intercept);
 
     private sealed record class HotKey(
         HOT_KEY_MODIFIERS FsModifiers,
